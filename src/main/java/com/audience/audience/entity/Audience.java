463ec2d92de6a0793a628b01848653cbc18ee22e
LorenadeParis <lorena.sanchez.mt@correo.usa.edu.co>
2021-11-09 20:46:50 -0600

    Ajustes Entities Reto 3 Final Final

--- src/main/java/com/audience/audience/entity/Audience.java
+++ src/main/java/com/audience/audience/entity/Audience.java
@@ -13,9 +13,9 @@ public class Audience implements Serializable  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String name;
     private String owner;
     private Integer capacity;
-    private String name;
     private String description;
     @ManyToOne
     @JoinColumn(name="category_id")
